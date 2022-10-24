3351563e03641ba592ef08ada0d9039d1d9fe373
Joris <je.tieleman@gmail.com>
2022-10-24 16:38:00 +0200

    added 2e solution algorithm Higest of four

--- Intermediate Algorithms/1 highest of four.js
+++ Intermediate Algorithms/1 highest of four.js
@@ -2,12 +2,19 @@ function largestOfFour(arr) {
     maxArray = []
     for (i = 0; i < arr.length; i++){
         thisArray = (arr[i])
-        max = Math.max.apply(null, thisArray);
-        console.log(max)
+        // // Solution 1
+        // max = Math.max.apply(null, thisArray);
+        // Solution 2
+        var max = 0;
+        for (let x = 0; x < thisArray.length; x++){
+            if (max < thisArray[x]){
+                max = thisArray[x]
+            }
+        }
         maxArray.push(max);
     }
     console.log(maxArray)
-    return maxArray;
+
   }
   largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
   //Returns [5,27,39,1001]
